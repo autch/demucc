04dbe07a2e618e4fd64581e96ac99bdb448f8527
Autch <autch@autch.net>
2014-08-08 01:05:27 +0900

    refactored too ugly code

--- demucc.c
+++ demucc.c
@@ -8,11 +8,11 @@
 #include <string.h>
 #include <stdint.h>
 
-#define LogL(format, ...) \
+#define LogL(format, ...)												\
 	fprintf(stderr, ("%s:%s(%d): " format "\n"), __FILE__, __func__, __LINE__, ##__VA_ARGS__)
 
 #define TPQN 24
-#define STACK_SIZE 256 // muslib §œ 24
+#define STACK_SIZE 256 // muslib „ÅØ 24
 
 struct pmd {
 	uint8_t* buffer;
@@ -24,6 +24,12 @@ struct pmd {
 
 	char* title;
 	char* title2;
+
+	int stack[STACK_SIZE];
+	int sp;
+
+	int len;
+	int drum_track;
 };
 
 uint16_t read_u16(uint8_t** pp)
@@ -46,14 +52,22 @@ uint8_t read_u8(uint8_t** pp)
 	return t;
 }
 
-void get_note(int note)
+int tick2beat(int tick)
+{
+	return (4 * TPQN) / tick;
+}
+
+void get_note(int note, int tick)
 {
 	// o4c = 60
 	char* key[] = {"c", "c+", "d", "d+", "e", "f", "f+", "g", "g+", "a", "a+", "b"};
 	int k = note % 12, oct = (note - 12) / 12;
 
-	printf("o%d%s", oct, key[k]);
+	printf("o%d%s%d", oct, key[k], tick2beat(tick));
 }
+
+int read_notes(struct pmd* pmd, uint8_t** pp);
+int read_commands(struct pmd* pmd, uint8_t n, uint8_t** pp);
 
 int main(int ac, char** av)
 {
@@ -61,6 +75,7 @@ int main(int ac, char** av)
 	struct pmd pmd;
 	char* filename = NULL;
 	uint8_t* p;
+	int i;
 
 	memset(&pmd, 0, sizeof pmd);
 
@@ -92,13 +107,9 @@ int main(int ac, char** av)
 
 	pmd.parts = calloc(pmd.num_parts, sizeof(uint8_t*));
 
-	{
-		int s = 1, i;
-
-		for(i = 0; i < pmd.num_parts; i++, s += 2) {
-			uint16_t addr = read_u16(&p);
-			pmd.parts[i] = pmd.buffer + addr;
-		}
+	for(i = 0; i < pmd.num_parts; i++) {
+		uint16_t addr = read_u16(&p);
+		pmd.parts[i] = pmd.buffer + addr;
 	}
 
 	pmd.drums = pmd.buffer + read_u16(&p);
@@ -106,234 +117,23 @@ int main(int ac, char** av)
 	pmd.title2 = (char*)(pmd.buffer + read_u16(&p));
 
 	printf("#Title %s\n", pmd.title);
-	printf("#Title2 %s\n", pmd.title2);
+	printf("#Title2 %s\n\n", pmd.title2);
 
-	{
-		uint16_t stack[STACK_SIZE];
-		int i;
-		for(i = 0; i < pmd.num_parts; i++) {
-			uint8_t* p = pmd.parts[i];
-			char partname = 'A' + i;
-			int len, drum_track;
-			int sp = 0;
+	for(i = 0; i < pmd.num_parts; i++) {
+		uint8_t** pp = pmd.parts + i;
+		char partname = 'A' + i;
 
-			printf("%c ", partname);
+		pmd.drum_track = 0;
+		pmd.sp = 0;
+		pmd.len = 0;
+
+		printf("%c ", partname);
 			
-			drum_track = 0;
-
-			for(;;) {
-				uint8_t n = read_u8(&p);
-
-				if(n >= 0xe0) {
-					int cmd = n - 0xe0;
-
-					switch(cmd) {
-					case 0x00:
-						// rest
-						printf("r");
-						break;
-					case 0x01:
-					{
-						// gate
-						int gate = read_u8(&p);
-						printf("q%d", -(24 * gate / 99 - 24));
-						break;
-					}
-					case 0x02:
-					{
-						// jump
-						uint16_t addr = read_u16(&p);
-						// FIXME: MML•≥•ﬁ•Û•…§»§∑§∆§¢§Î°©
-						p = pmd.buffer + addr;
-						break;
-					}
-					case 0x03:
-					{
-						// call
-						uint16_t addr = read_u16(&p);
-						uint8_t times = read_u8(&p);
-						uint16_t here = p - pmd.buffer;
-						
-						stack[sp++] = times;
-						stack[sp++] = here;
-						stack[sp++] = addr;
-						
-						p = pmd.buffer + addr;
-
-						break;
-					}
-					case 0x04:
-						// repeat
-						printf("[%d", read_u8(&p)); // FIXME: •Î°º•◊≤ÛøÙ§œ next §ÀΩÒ§´§ §§§»§§§±§ §§
-						break;
-					case 0x05:
-						// next
-						printf("]");
-						break;
-					case 0x06:
-						// transpose
-						printf("_%d", read_u8(&p)); // FIXME: √±∞Ã§¨§™§´§∑§§
-						break;
-					case 0x07:
-						// tempo
-						printf("T%d", read_u8(&p));
-						break;
-					case 0x08:
-						// inst
-						printf("@%d", read_u8(&p));
-						break;
-					case 0x09:
-						// volume
-						printf("V%d", read_u8(&p));
-						break;
-					case 0x0a:
-					{
-						// envelope
-						int ar, dr, sl, sr;
-						ar = read_u8(&p);
-						dr = read_u8(&p);
-						sl = read_u8(&p);
-						sr = read_u8(&p);
-						printf("E%d,%d,%d,%d", ar, dr, sl, sr);
-						break;
-					}
-					case 0x0b:
-						// detune
-						printf("D%d", read_u8(&p)); // FIXME: √±∞Ã§¨ —
-						break;
-					case 0x0c:
-					{
-						// note 2
-						int note;
-
-						note = read_u8(&p);
-						if(note >= 0x80) note -= 0x80 - 12;
-						
-						get_note(note);
-						break;
-					}
-					case 0x0d:
-						// portamento params
-						printf("P%d,%d", read_u8(&p), read_u8(&p));
-						break;
-					case 0x0e:
-						// portamento on
-						printf("{");
-						break;
-					case 0x0f:
-						// portamento off
-						printf("}");
-						break;
-					case 0x10:
-						// drum track mode
-						printf("RT%d", read_u8(&p));
-						drum_track = 1;
-						break;
-					case 0x11:
-					{
-						// vibrato
-						int depth, speed, delay, rate;
-						depth = read_u8(&p);
-						speed = read_u8(&p);
-						delay = read_u8(&p);
-						rate = read_u8(&p);
-						printf("Y%d,%d,%d,%d", depth, speed, delay, rate);
-						break;
-					}
-					case 0x12:
-						// master volume
-						read_u8(&p);
-						break;
-					case 0x13:
-						// master fade
-						read_u16(&p);
-						break;
-					case 0x14:
-						// part fade
-						read_u16(&p);
-						break;
-					case 0x15:
-						// bend
-						read_u16(&p);
-						break;
-					case 0x16:
-						// break
-						printf(":");
-						break;
-					case 0x17:
-						// nop
-						break;
-					case 0x18:
-						// legato on
-						printf("&"); // FIXME: mucc§«•≥•Û•—•§•Î§«§≠§ §§
-						break;
-					case 0x19:
-						// legato off
-						printf("!"); // FIXME: §…§≥§«ª»§¶°©
-						break;
-					case 0x1a:
-						// expression
-						printf("v%d", read_u8(&p));
-						break;
-					case 0x1b:
-					{
-						int8_t vol = read_u8(&p);
-						// rel. expression
-						if(vol >= 0) {
-							printf("v+%d", vol);
-						} else {
-							printf("v%d", vol);
-						}
-						break;
-					}
-					case 0x1c:
-					case 0x1d:
-					case 0x1e:
-					case 0x1f:
-					default:
-						// nop
-						break;
-					}
-				} else if(n >= 0x80) {
-					if(drum_track == 1) {
-						int index = n - 0x80;
-						uint8_t* pp = pmd.drums + (index << 1);
-						uint16_t addr = read_u16(&pp);
-
-						stack[sp++] = 1;
-						stack[sp++] = p - pmd.buffer;
-						stack[sp++] = addr;
-						p = pmd.buffer + addr;
-					} else {
-						int note = n - 0x80 + 12;
-						get_note(note);
-					}					
-				} else if(n != 0) {
-					if(n == 127) {
-						n = read_u8(&p);
-						if(n < 127) n += 256;
-					}
-					len = n;
-					printf("l%d", (4 * TPQN) / len);
-				} else {
-					// n == 0
-					int lsp = sp;
-					if(lsp == 0) {
-						break;
-					}
-
-					lsp -= 3;
-					if(--stack[lsp] != 0) {
-						p = pmd.buffer + stack[lsp + 2];
-					} else {
-						p = pmd.buffer + stack[lsp + 1];
-						sp = lsp;
-					}
-				}
-			}
-
-			printf("\n\n; ====\n\n");
+		while(!read_notes(&pmd, pp)) {
+			//
 		}
+
+		printf("\n\n; ====\n\n");
 	}
 
 	free(pmd.parts);
@@ -342,4 +142,226 @@ int main(int ac, char** av)
 	return 0;
 }
 
+/**
+ * @return „Éë„Éº„ÉàÁµÇ‰∫Ü„ÅÆ„Å®„Åç 1, „Åù„ÅÜ„Åß„Å™„ÅÑ„Å®„Åç 0
+ */
+int read_notes(struct pmd* pmd, uint8_t** pp)
+{
+	uint8_t n = read_u8(pp);
 
+	if(n >= 0xe0) {
+		read_commands(pmd, n, pp);
+	} else if(n >= 0x80) {
+		if(pmd->drum_track == 1) {
+			int index = n - 0x80;
+			uint8_t* pd = pmd->drums + (index << 1);
+			uint16_t addr = read_u16(&pd);
+			
+			pmd->stack[pmd->sp++] = 1;
+			pmd->stack[pmd->sp++] = *pp - pmd->buffer;
+			pmd->stack[pmd->sp++] = addr;
+			*pp = pmd->buffer + addr;
+			
+			printf(" ");
+		} else {
+			int note = n - 0x80 + 12;
+			get_note(note, pmd->len);
+		}					
+	} else if(n != 0) {
+		if(n == 127) {
+			n = read_u8(pp);
+			if(n < 127) n += 256;
+		}
+		pmd->len = n;
+		// printf("l%d", tick2beat(pmd.len));
+	} else {
+		// n == 0
+		int lsp = pmd->sp;
+		if(lsp == 0) {
+			return 1;
+		}
+		
+		lsp -= 3;
+		if(--pmd->stack[lsp] != 0) {
+			*pp = pmd->buffer + pmd->stack[lsp + 2];
+		} else {
+			*pp = pmd->buffer + pmd->stack[lsp + 1];
+			pmd->sp = lsp;
+		}
+	}
+	return 0;
+}
+
+int read_commands(struct pmd* pmd, uint8_t n, uint8_t** pp)
+{
+	int cmd = n - 0xe0;
+
+	switch(cmd) {
+	case 0x00:
+		// rest
+		printf("r%d", tick2beat(pmd->len));
+		break;
+	case 0x01:
+	{
+		// gate
+		int gate = read_u8(pp);
+		printf("q%d", -(24 * gate / 99 - 24));
+		break;
+	}
+	case 0x02:
+	{
+		// jump
+		uint16_t addr = read_u16(pp);
+		// FIXME: MML„Ç≥„Éû„É≥„Éâ„Å®„Åó„Å¶„ÅÇ„ÇãÔºü
+		*pp = pmd->buffer + addr;
+		break;
+	}
+	case 0x03:
+	{
+		// call
+		uint16_t addr = read_u16(pp);
+		uint8_t times = read_u8(pp);
+		uint16_t here = *pp - pmd->buffer;
+						
+		pmd->stack[pmd->sp++] = times;
+		pmd->stack[pmd->sp++] = here;
+		pmd->stack[pmd->sp++] = addr;
+						
+		*pp = pmd->buffer + addr;
+		break;
+	}
+	case 0x04:
+		// repeat
+		printf("[%d", read_u8(pp)); // FIXME: „É´„Éº„ÉóÂõûÊï∞„ÅØ next „Å´Êõ∏„Åã„Å™„ÅÑ„Å®„ÅÑ„Åë„Å™„ÅÑ
+		break;
+	case 0x05:
+		// next
+		printf("]");
+		break;
+	case 0x06:
+		// transpose
+		printf("_%d", read_u8(pp)); // FIXME: Âçò‰Ωç„Åå„Åä„Åã„Åó„ÅÑ
+		break;
+	case 0x07:
+		// tempo
+		printf("T%d", read_u8(pp));
+		break;
+	case 0x08:
+		// inst
+		printf("@%d", read_u8(pp));
+		break;
+	case 0x09:
+		// volume
+		printf("V%d", read_u8(pp));
+		break;
+	case 0x0a:
+	{
+		// envelope
+		int ar, dr, sl, sr;
+		ar = read_u8(pp);
+		dr = read_u8(pp);
+		sl = read_u8(pp);
+		sr = read_u8(pp);
+		printf("E%d,%d,%d,%d", ar, dr, sl, sr);
+		break;
+	}
+	case 0x0b:
+		// detune
+		printf("D%d", read_u8(pp)); // FIXME: Âçò‰Ωç„ÅåÂ§â
+		break;
+	case 0x0c:
+	{
+		// note 2
+		int note;
+
+		note = read_u8(pp);
+		if(note >= 0x80) note -= 0x80 - 12;
+						
+		get_note(note, pmd->len);
+		break;
+	}
+	case 0x0d:
+		// portamento params
+		printf("P%d,%d", read_u8(pp), read_u8(pp));
+		break;
+	case 0x0e:
+		// portamento on
+		printf("{");
+		break;
+	case 0x0f:
+		// portamento off
+		printf("}");
+		break;
+	case 0x10:
+		// drum track mode
+		printf("RT%d", read_u8(pp));
+		pmd->drum_track = 1;
+		break;
+	case 0x11:
+	{
+		// vibrato
+		int depth, speed, delay, rate;
+		depth = read_u8(pp);
+		speed = read_u8(pp);
+		delay = read_u8(pp);
+		rate = read_u8(pp);
+		printf("Y%d,%d,%d,%d", depth, speed, delay, rate);
+		break;
+	}
+	case 0x12:
+		// master volume
+		read_u8(pp);
+		break;
+	case 0x13:
+		// master fade
+		read_u16(pp);
+		break;
+	case 0x14:
+		// part fade
+		read_u16(pp);
+		break;
+	case 0x15:
+		// bend
+		read_u16(pp);
+		break;
+	case 0x16:
+		// break
+		printf(":");
+		break;
+	case 0x17:
+		// nop
+		break;
+	case 0x18:
+		// legato on
+		printf("&"); // FIXME: mucc„Åß„Ç≥„É≥„Éë„Ç§„É´„Åß„Åç„Å™„ÅÑ
+		break;
+	case 0x19:
+		// legato off
+		printf("!"); // FIXME: „Å©„Åì„Åß‰Ωø„ÅÜÔºü
+		break;
+	case 0x1a:
+		// expression
+		printf("v%d", read_u8(pp));
+		break;
+	case 0x1b:
+	{
+		int8_t vol = read_u8(pp);
+		// rel. expression
+		if(vol >= 0) {
+			printf("v+%d", vol);
+		} else {
+			printf("v%d", vol);
+		}
+		break;
+	}
+	case 0x1c:
+	case 0x1d:
+	case 0x1e:
+	case 0x1f:
+	default:
+		// nop
+		break;
+	}
+
+	return 0;
+}
